a509df089209d65c41a4c643a92e30dc8a540d4d
labCap <banlazthosow@gamil.com>
2022-10-20 20:38:08 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<Header />
+
+			<main className="main">
+				<section className="section">
+					<div className="container">
+						<div className="section__inner">
+							<h2 className="section__title">Style & Beauty</h2>
+							<h3 className="section__subtitle">New fashion brand</h3>
+							<a href="#" className="btn">
+								VIEW PROJECT
+							</a>
+						</div>
+					</div>
+				</section>
+
+				<section className="start">
+					<div className="container">
+						<div className="start__inner">
+							<h2 className="start__title">Let's do something amazing.</h2>
+							<h3 className="start__subtitle">Get in touch with us today</h3>
+							<a href="#" className="start__btn">
+								START A PROJECT
+							</a>
+						</div>
+					</div>
+				</section>
+			</main>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
