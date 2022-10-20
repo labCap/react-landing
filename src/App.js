38fa2593fc085c4d1d87c4fe248f0299d4cc6318
labCap <banlazthosow@gamil.com>
2022-10-20 21:48:34 +0300

    bag-fix

--- src/App.js
+++ src/App.js
@@ -9,27 +9,27 @@ import img4 from "./img/4.jpg";
 import img5 from "./img/5.jpg";
 
 const sectionList = [
+	{ id: 1, title: "Style & Beauty", subtitle: "New fashion brand", img: img1 },
 	{
-		title: "Style & Beauty",
-		subtitle: "New fashion brand",
-		img: img1,
-	},
-	{
+		id: 2,
 		title: "Living Room Girls",
 		subtitle: "Interior Design - Red Room",
 		img: img2,
 	},
 	{
+		id: 3,
 		title: "The shape of water flowers",
 		subtitle: "Book Cover",
 		img: img3,
 	},
 	{
+		id: 4,
 		title: "Capturing the sunlight",
 		subtitle: "Fashion Photoshoot",
 		img: img4,
 	},
 	{
+		id: 5,
 		title: "Looking on the other side",
 		subtitle: "Portrait photography",
 		img: img5,
@@ -43,7 +43,12 @@ function App() {
 
 			<main className="main">
 				{sectionList.map((e) => (
-					<Section title={e.title} subtitle={e.subtitle} bgImg={e.img} />
+					<Section
+						key={e.id.toString()}
+						title={e.title}
+						subtitle={e.subtitle}
+						bgImg={e.img}
+					/>
 				))}
 
 				<section className="start">
